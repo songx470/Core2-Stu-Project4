f1bb67957c822ca266cd452daa9b89b2f1b34308
songx470 <70958823+songx470@users.noreply.github.com>
2021-05-05 22:12:35 -0700

    sns

--- script.js
+++ script.js
@@ -1,43 +1,60 @@
+$("#input1").keyup(function() {
+  $("#wordone").html($("#input1").val());
+});
 
-  function addShadows (sel, num_of_shad) {
-    var shadows = sel+'{text-shadow: ';
-    var font_size = $(sel).css('font-size').slice(0,-2);
-    while(--num_of_shad){
-      shadows += '0 '+ (-font_size*num_of_shad) + 'px 0 rgba(0,0,0,'+(1/(num_of_shad+0.1))+'),';
-      
-    }
+$("#input2").keyup(function() {
+  $("#wordtwo").html($("#input2").val());
+});
 
-    // what follows is kinda nasty :(
-    var styleTag = $('style').html();
-    $('style').remove();
-    $('html > head').append('<style>'+styleTag+shadows.slice(0,-1) + ";}"+'</style>');
-  }
+$("#fontbox1").keyup(function() {
+  $("#wordone").css("font-size", $("#fontbox1").val() + "px");
+});
 
-  function rotate (jqSel) {
-      
-    var letters = $(jqSel).html().split('');
-    var n = letters.length;
-    var deg = 360 / n;
-    for (var i = 0; i < n; i++) {
-      var TRANSF = "transform: rotate("+Math.round(deg*i)+"deg);";
-      var el = $('<span/>', {
-          'class': 'rot-' + i, 
-          text: letters[i],
-          style: TRANSF + '-webkit-' + TRANSF});
-      $(jqSel).before(el);
-    }
-    $(jqSel).remove();
-  }
+$("#fontbox2").keyup(function() {
+  $("#wordtwo").css("font-size", $("#fontbox2").val() + "px");
+});
 
-  rotate('#rotated');
-  addShadows('span[class^=rot-]', 4);
+$(".sq").click(function() {
+  var col;
+  col = $(this).attr("name");
+  $(".two").removeClass("pink");
+  $(".two").removeClass("blue");
+  $(".two").removeClass("green");
+  $(".two").removeClass("winner");
 
-  $('#in').on('keyup', function (e) {
-    $('span[class^=rot-]').remove();
-    var newEl = $('<span></span', {id: 'rotated', text: $(this).val()})
-    $('body').append(newEl);
+  $(".two").addClass(col);
+  $(".sq").removeClass("on");
+  $(this).addClass("on");
+})
+$(function() {
 
-    rotate('#rotated');
-    //console.log();
-    addShadows('span[class^=rot-]', $('#in-number').val());
+  $(".control").draggable();
+  $("#wordone").draggable({
+    grid: [20, 20]
   });
+  $("#wordtwo").draggable({
+    grid: [20, 20]
+  });
+
+});
+
+$("#renderbutton").click(function() {
+
+  html2canvas($(".one"), {
+    useCORS: true,
+    onrendered: function(canvas) {
+      // canvas is the final rendered <canvas> element
+      var myImage = canvas.toDataURL("image/png");
+      window.open(myImage);
+    }
+  });
+});
+
+
+$(".shitty").click(function() {
+  if ($(this).hasClass("shit")) {
+    $("#wordtwo").removeClass("shit");
+  } else {
+    $("#wordtwo").addClass("shit");
+  };
+})
