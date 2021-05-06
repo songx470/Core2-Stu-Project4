de5272089fdbb2ff6b0d9fe0f64e3f4db91dadf9
Ricky Xie <xier892@newschool.edu>
2021-05-06 02:25:05 -0400

    is this what you wanted?

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
-$("#input1").keyup(function() {
-  $("#wordone").html($("#input1").val());
+$('#input1').on('input', function () {
+	$('input[name="sticker"]:checked + .two #wordone').html($(this).val());
 });
 
-$("#input2").keyup(function() {
-  $("#wordtwo").html($("#input2").val());
+$('#input2').on('input', function () {
+	$('input[name="sticker"]:checked + .two #wordtwo').html($(this).val());
 });
 
-$("#fontbox1").keyup(function() {
-  $("#wordone").css("font-size", $("#fontbox1").val() + "px");
+$('#fontbox1').on('input', function () {
+	$('input[name="sticker"]:checked + .two #wordone').css('font-size', `${$(this).val()}px`);
+	$('input[name="sticker"]:checked + .two #wordone').attr('data-font-size', $(this).val());
 });
 
-$("#fontbox2").keyup(function() {
-  $("#wordtwo").css("font-size", $("#fontbox2").val() + "px");
-});
-
-$(".sq").click(function() {
-  var col;
-  col = $(this).attr("name");
-  $(".two").removeClass("pink");
-  $(".two").removeClass("blue");
-  $(".two").removeClass("green");
-  $(".two").removeClass("winner");
-
-  $(".two").addClass(col);
-  $(".sq").removeClass("on");
-  $(this).addClass("on");
+$('#fontbox2').on('input', function () {
+	$('input[name="sticker"]:checked + .two #wordtwo').css('font-size', `${$(this).val()}px`);
+	$('input[name="sticker"]:checked + .two #wordtwo').attr('data-font-size', $(this).val());
 })
+
+$('input[name="color"]').change(function () {
+	$('input[name="sticker"]:checked').attr('data-color', $(this).val());
+	$('input[name="sticker"]:checked + .two').removeClass('pink blue green winner');
+	$('input[name="sticker"]:checked + .two').addClass($(this).val());
+});
+
+$('input[name="sticker"]').change(function () {
+	$('#input1').val($('input[name="sticker"]:checked + .two #wordone').text().length > 0 ? $('input[name="sticker"]:checked + .two #wordone').text() : '');
+	$('#input2').val($('input[name="sticker"]:checked + .two #wordtwo').text().length > 0 ? $('input[name="sticker"]:checked + .two #wordtwo').text() : '');
+	$('#fontbox1').val($('input[name="sticker"]:checked + .two #wordone').attr('data-font-size') || '80');
+	$('#fontbox2').val($('input[name="sticker"]:checked + .two #wordtwo').attr('data-font-size') || '140');
+	$('input[name="color"][value="pink"]').prop('checked', true);
+	$(`input[name="color"][value="${$('input[name="sticker"]:checked').attr('data-color')}"]`).prop('checked', true);
+});
+
 $(function() {
 
   $(".control").draggable();
@@ -37,4 +42,3 @@ $(function() {
   });
 
 });
-
